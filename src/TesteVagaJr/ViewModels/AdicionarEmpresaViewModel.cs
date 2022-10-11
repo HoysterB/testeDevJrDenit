ef77ade9cb80d87a9d5ee4ef8406789c906be084
HoysterB <ataumaturgo61@gmail.com>
2022-10-11 11:45:13 -0300

    feature2

--- src/TesteVagaJr/ViewModels/AdicionarEmpresaViewModel.cs
+++ src/TesteVagaJr/ViewModels/AdicionarEmpresaViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace TesteVagaJr.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteVagaJr.ViewModels;
 
 public class AdicionarEmpresaViewModel
 {
-    
+    [Required(ErrorMessage = "A uf não pode ser vazia.")]
+    [MinLength(2, ErrorMessage = "A uf deve ter no mínimo 2 caracteres.")]
+    [MaxLength(2,ErrorMessage = "A uf deve ter no máximo 2 carateres.")]
+    public string Uf { get; private set; }
+    [Required]
+    public string NomeFantasia { get; private set; }
+    public string Cnpj { get; private set; }
 }
